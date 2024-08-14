874e04fe06315347dd040c61eb91d9b35e20894c
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-14 13:49:32 +0200

    updated view.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Book, Library  # Import the models
+from django.shortcuts import render
+from .models import Book, Library  # Import the Book and Library models
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()  # Retrieve all Book instances from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Function-based view to display details for a specific library
 def library_detail(request, pk):
     library = get_object_or_404(Library, pk=pk)  # Fetch the library by primary key
-    books = library.books.all()  # Get all books associated with this library
+    books = library.books.all()  # Retrieve all books associated with this library
     context = {
         'library': library,
         'books': books
