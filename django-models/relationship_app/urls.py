bd19e1137454e19983ce46083d76c73e61e8791b
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-14 14:18:02 +0200

    Corrected

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, register
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from .views import list_books
+from .views import LibraryDetailView
+from .views import register
 
 urlpatterns = [
     # Function-based view for listing books
@@ -12,8 +15,9 @@ urlpatterns = [
     # Authentication views
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),  # Link to custom registration view
+    path('register/', register, name='register'),  # Custom view for user registration
 ]
+
 
 
 
