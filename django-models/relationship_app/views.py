63711f04a17216bc11e6ec8321cbc9e765537e64
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-14 16:45:01 +0200

    Updated files

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,11 +1,9 @@
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
-# Custom registration view
+# Registration view using Django's built-in UserCreationForm
 class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = 'register.html'
