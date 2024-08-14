ac02c7d2cdcfecb1ba115791ae3a336c151cfd31
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-14 13:14:58 +0200

    query_samples.py updated

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,12 +1,19 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
+# Query all books by a specific author using the direct filter
 author_name = "Author Name"  # replace with an actual author name
 author = Author.objects.get(name=author_name)
-books_by_author = author.books.all()
+books_by_author = Book.objects.filter(author=author)
 print(f"Books by {author_name}:")
 for book in books_by_author:
     print(book.title)
+
+# Alternatively, using the reverse relationship from the Author model
+# Uncomment the following lines if you want to use the reverse relationship instead
+# books_by_author = author.books.all()
+# print(f"Books by {author_name}:")
+# for book in books_by_author:
+#     print(book.title)
 
 # List all books in a library
 library_name = "Library Name"  # replace with an actual library name
