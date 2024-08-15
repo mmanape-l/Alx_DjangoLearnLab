77614bb3f4c6071c137df986627ac688346e44c4
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-15 15:09:24 +0200

    view.py updated

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm  # Add this import
 
 # Custom LoginView
 class CustomLoginView(LoginView):
@@ -37,14 +38,14 @@ def is_member(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, "admin_view.html")
+    return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, "librarian_view.html")
+    return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, "member_view.html")
+    return render(request, 'relationship_app/member_view.html')
 
 
