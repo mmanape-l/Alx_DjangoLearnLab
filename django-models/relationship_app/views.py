0a3cba3761efa6b386547b324146e586adec0e3e
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-15 14:57:21 +0200

    views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -37,14 +37,14 @@ def is_member(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request,'admin_view.html')
+    return render(request, "admin_view.html")
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request,'librarian_view.html')
+    return render(request, "librarian_view.html")
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request,'member_view.html')
+    return render(request, "member_view.html")
 
 
