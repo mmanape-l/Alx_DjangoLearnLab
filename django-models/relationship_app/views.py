79cf78690b3aaa5e099166205b34746219c8c6dd
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-15 13:44:25 +0200

    Added views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 
 def user_login(request):
@@ -9,7 +11,21 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('home')  # Redirect to a home page or dashboard
+            return redirect('home')  # Adjust the redirect target as needed
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'logout.html')
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
