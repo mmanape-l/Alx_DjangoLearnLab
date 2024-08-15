326b4ab2b3e2438acbfcda5b2607d7fb80ca9802
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-15 13:50:17 +0200

    updated url.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import user_register
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('register/', views.user_register, name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', user_register, name='register'),
 ]
