ef2c4e060df769fed2deee4e02d4e98dbd0d9541
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-15 14:11:52 +0200

    updated views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,31 +1,30 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 from django.contrib.auth import login
-from django.contrib.auth import logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
-def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home')  # Adjust the redirect target as needed
-    else:
-        form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.get_user()
+        login(self.request, user)
+        return response
 
-def user_logout(request):
-    logout(request)
-    return render(request, 'logout.html')
+class CustomLogoutView(LogoutView):
+    template_name = 'logout.html'
 
-def user_register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.save()
+        login(self.request, user)
+        return response
+
