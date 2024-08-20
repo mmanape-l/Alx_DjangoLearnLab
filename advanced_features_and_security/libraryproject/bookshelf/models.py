d3e5abacf1d3bff4cd15885225388762c7a92485
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-20 15:41:01 +0200

    model.py updated

--- advanced_features_and_security/libraryproject/bookshelf/models.py
+++ advanced_features_and_security/libraryproject/bookshelf/models.py
@@ -1,10 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+class CustomUser(AbstractUser):
+    # Add any additional fields you want for your custom user model
+    # For example:
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
 
     def __str__(self):
         return self.title
-
