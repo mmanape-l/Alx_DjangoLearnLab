6b571c4b0d1eb94b9421c9daf0eb0cf54ce49450
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-22 19:12:08 +0200

    views.py updated

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm, ExampleForm  # Add ExampleForm import here
+from .forms import BookForm, ExampleForm  # Ensure ExampleForm is imported correctly
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
