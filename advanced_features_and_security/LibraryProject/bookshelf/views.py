c27dfd1f70eeb1f7c7116dac715be98de70085a6
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-22 18:51:53 +0200

    templates added

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 
-@permission_required('yourapp.can_view', raise_exception=True)
+@permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'book_list.html', {'books': books})
 
-@permission_required('yourapp.can_create', raise_exception=True)
+@permission_required('bookshelf.can_create', raise_exception=True)
 def book_create(request):
     if request.method == 'POST':
         # Handle form submission
         pass
     return render(request, 'book_form.html')
 
-@permission_required('yourapp.can_edit', raise_exception=True)
+@permission_required('bookshelf.can_edit', raise_exception=True)
 def book_edit(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
@@ -22,7 +22,7 @@ def book_edit(request, pk):
         pass
     return render(request, 'book_form.html', {'book': book})
 
-@permission_required('yourapp.can_delete', raise_exception=True)
+@permission_required('bookshelf.can_delete', raise_exception=True)
 def book_delete(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
