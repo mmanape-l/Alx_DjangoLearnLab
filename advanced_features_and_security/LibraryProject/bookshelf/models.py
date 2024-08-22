36c633d7a874b7f828876d896e666482628f23f2
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-22 15:13:58 +0200

    models.py updated

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -2,9 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # Add any additional fields you want for your custom user model
-    # For example:
     date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
     def __str__(self):
         return self.username
