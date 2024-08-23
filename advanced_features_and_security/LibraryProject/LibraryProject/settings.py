b4d9ac0f817d4d98110050167a446bec763f600d
mmanape-l <mmanape.letlalo@gmail.com>
2024-08-23 12:18:10 +0200

    settings.py updated

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -19,9 +19,14 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True  # Optional, prevents client-side access to CSRF cookies
 SESSION_COOKIE_HTTPONLY = True  # Optional, prevents client-side access to session cookies
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
 
 # Redirect all HTTP requests to HTTPS
 SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 # Application definition
 INSTALLED_APPS = [
