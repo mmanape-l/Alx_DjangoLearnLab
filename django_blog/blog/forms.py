cf6ec5fd037dac7dc932a1cf19748f0dab50f086
mmanape-l <mmanape.letlalo@gmail.com>
2024-09-10 13:55:45 +0200

    updated templates

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,16 +2,16 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
