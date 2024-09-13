e2a45fde4651c9684e4bb4e13cc23f5385fd833f
mmanape-l <mmanape.letlalo@gmail.com>
2024-09-13 17:35:52 +0200

    Updated url.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,9 +6,9 @@ from .views import (
     PostUpdateView, 
     PostDeleteView, 
     profile,
-    create_comment,
-    update_comment,
-    delete_comment
+    CommentCreateView,
+    CommentUpdateView,
+    CommentDeleteView
 )
 
 urlpatterns = [
@@ -17,13 +17,13 @@ urlpatterns = [
 
     # Blog post CRUD operations
     path('', PostListView.as_view(), name='post-list'),  # List all posts
-    path('post/<int:pk>/update/', PostDetailView.as_view(), name='post-detail'),  # View a single post
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # View a single post
     path('post/new/', PostCreateView.as_view(), name='post-create'),  # Create a new post
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  # Edit a post
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete a post
 
     # Comment-related actions
-    path('post/<int:post_id>/comments/new/', create_comment, name='add-comment'),  # Add a new comment
-    path('post/<int:post_id>/comments/<int:comment_id>/edit/', update_comment, name='edit-comment'),  # Edit a comment
-    path('post/<int:post_id>/comments/<int:comment_id>/delete/', delete_comment, name='delete-comment'),  # Delete a comment
+    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add-comment'),  # Add a new comment
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='edit-comment'),  # Edit a comment
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),  # Delete a comment
 ]
