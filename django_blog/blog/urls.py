09c2cf9a6f2eabe961b1f08246d0aa4da5c4df5c
mmanape-l <mmanape.letlalo@gmail.com>
2024-09-13 13:51:58 +0200

    urls.py updated

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     # Blog post CRUD operations
     path('', PostListView.as_view(), name='post-list'),  # List all posts
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # View a single post
+    path('post/<int:pk>/update/', PostDetailView.as_view(), name='post-detail'),  # View a single post
     path('post/new/', PostCreateView.as_view(), name='post-create'),  # Create a new post
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  # Edit a post
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete a post
