3469fa484cde2e98b2c48b064de5880b7c17a2a3
mmanape-l <mmanape.letlalo@gmail.com>
2024-09-13 18:35:29 +0200

    fixed views.py again

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -22,19 +22,29 @@ def profile(request):
 # ListView for displaying all posts
 class PostListView(ListView):
     model = Post
-    template_name = 'post_list.html'  # Custom template for listing posts
-    context_object_name = 'posts'  # Name of the context object to use in the template
-    ordering = ['-published_date']  # Display posts in descending order of published date
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('search')
+        if search_query:
+            queryset = Post.objects.filter(
+                Q(title__icontains=search_query) |
+                Q(tags__name__icontains=search_query) |
+                Q(content__icontains=search_query)
+            ).distinct()
+        return queryset
 
 # DetailView for displaying a single post along with comments
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'post_detail.html'  # Custom template for viewing a single post
-    context_object_name = 'post'  # Name of the context object to use in the template
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Add the comment form to the context
         if self.request.user.is_authenticated:
             context['comment_form'] = CommentForm()
             context['comments'] = Comment.objects.filter(post=self.object)
@@ -43,23 +53,31 @@ class PostDetailView(DetailView):
 # CreateView for creating a new post
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    form_class = PostForm  # Use PostForm to handle post creation
-    template_name = 'post_form.html'  # Custom form template
-    success_url = reverse_lazy('post-list')  # Redirect after successful creation
+    form_class = PostForm
+    template_name = 'post_form.html'
+    success_url = reverse_lazy('post-list')
 
     def form_valid(self, form):
-        form.instance.author = self.request.user  # Automatically set the author to the logged-in user
+        form.instance.author = self.request.user
         return super().form_valid(form)
 
 # UpdateView for editing an existing post
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    form_class = PostForm  # Use PostForm to handle post updates
-    template_name = 'post_form.html'  # Reuse the same form template as for creation
-    success_url = reverse_lazy('post-list')  # Redirect after successful update
+    form_class = PostForm
+    template_name = 'post_form.html'
+    success_url = reverse_lazy('post-list')
 
     def test_func(self):
         post = self.get_object()
-        return self.request.user == post.author  # Ensure only the author can edit the post
+        return self.request.user == post.author
 
-# DeleteView
+# DeleteView for deleting a post
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
