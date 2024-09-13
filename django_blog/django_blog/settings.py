a3f6d5f6525b45249859041c7937b16829124d2d
mmanape-l <mmanape.letlalo@gmail.com>
2024-09-13 18:43:19 +0200

    updated settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -59,6 +59,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',  # Leave empty for SQLite
+        'PASSWORD': '',  # Leave empty for SQLite
+        'HOST': '',  # Leave empty for SQLite
+        'PORT': '',  # Leave empty for SQLite
     }
 }
 
