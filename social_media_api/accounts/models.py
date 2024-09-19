a943e7c13cb2235eaebb5109d0f5344aa2824b5e
Mmanape Letlalo <mmanape.letlalo@gmail.com>
2024-09-19 13:14:43 +0200

    corrected files

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,34 +1,10 @@
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-User = get_user_model()
+class CustomUser(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
-
-    class Meta:
-        model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 'bio')
-        extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True}
-        }
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            bio=validated_data.get('bio', '')
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+    def __str__(self):
+        return self.username
