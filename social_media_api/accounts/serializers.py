57b67bf607f567562134cb9a9676f95e227d396b
Mmanape Letlalo <mmanape.letlalo@gmail.com>
2024-09-19 13:24:40 +0200

    Updated

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -8,14 +8,13 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
+    email = serializers.CharField(required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
 
     class Meta:
         model = User
         fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 'bio')
-        extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True}
-        }
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -23,14 +22,15 @@ class UserSerializer(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
-            password=validated_data['password'],
-            bio=validated_data.get('bio', '')
+            password=validated_data['password']
         )
+        user.bio = validated_data.get('bio', '')
+        user.save()
         Token.objects.create(user=user)
         return user
 
