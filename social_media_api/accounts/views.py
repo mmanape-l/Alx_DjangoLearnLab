44adb155bd7439ceeb82862ce29a8073e1c611f0
Mmanape Letlalo <mmanape.letlalo@gmail.com>
2024-09-22 15:22:02 +0200

    Updated urls.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets, permissions
-from rest_framework.decorators import action
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from .models import CustomUser
@@ -7,19 +6,23 @@ from .serializers import CustomUserSerializer
 
 User = get_user_model()
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserList(generics.ListAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    @action(detail=False, methods=['post'], url_path='follow/(?P<user_id>[^/.]+)')
-    def follow_user(self, request, user_id=None):
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
         user_to_follow = CustomUser.objects.get(id=user_id)
         request.user.following.add(user_to_follow)
         return Response({"message": "You are now following this user."})
 
-    @action(detail=False, methods=['post'], url_path='unfollow/(?P<user_id>[^/.]+)')
-    def unfollow_user(self, request, user_id=None):
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
         user_to_unfollow = CustomUser.objects.get(id=user_id)
         request.user.following.remove(user_to_unfollow)
         return Response({"message": "You have unfollowed this user."})
