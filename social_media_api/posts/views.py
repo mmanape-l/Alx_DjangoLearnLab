269ea34e5ca145762d15ce116f7c72a859d7e220
Mmanape Letlalo <mmanape.letlalo@gmail.com>
2024-09-22 15:29:26 +0200

    updated views.py and urls.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from django.contrib.auth import get_user_model
@@ -6,7 +6,21 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # Get the users that the current user follows
+        followed_users = user.following.all()  
+        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
